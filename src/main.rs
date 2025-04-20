c759f32f021daa691e82f1f98e840ed23151c9e2
lonely43 <iladenisov327@gmail.com>
2025-04-20 18:45:53 +0300

    [feat]: create paths_manager

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[path ="./scripts/get_paths.rs"]
+mod paths_mod; // get paths of apps
+
+use paths_mod::paths_manager;
+
 fn main() {
     println!("Hello, world!");
+
+    let paths = paths_manager::get_paths();
+    print!("{}", paths.unwrap().zapret)
 }
---
+++ src/scripts/get_paths.rs
@@ -0,0 +1,30 @@
+pub mod paths_manager {
+   use std::io::{self, Write};
+   
+   pub struct Paths {
+      pub discord: String, 
+      pub zapret: String
+   }
+
+   pub fn get_paths() -> Result<Paths, io::Error> {
+      let mut paths = Paths {
+         discord: String::new(),
+         zapret: String::new()
+      };
+
+      fn get_path(promt: &str) -> Result<String, io::Error>{
+         print!("{}", promt);
+         io::stdout().flush()?;
+
+         let mut input = String::new();
+         io::stdin().read_line(&mut input)?;
+
+         Ok(input.trim().to_string())
+      }
+
+      paths.discord = get_path("Enter global path for Discord: ")?;
+      paths.zapret = get_path("Enter global path for Zapret-Discord: ")?;
+
+      Ok(paths)
+   }
+}
