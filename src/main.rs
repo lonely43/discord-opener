78e42ef3f640aa41f4c2c870503502950c684ffd
lonely43 <iladenisov327@gmail.com>
2025-05-04 21:08:09 +0300

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use std::process::Command;
+
 #[path ="./scripts/get_paths.rs"]
 mod paths_mod; // get paths of apps
 
 use paths_mod::paths_manager;
 
 fn main() {
-    println!("Hello, world!");
+    //let paths = paths_manager::get_paths().unwrap(); // make error handler 
 
-    let paths = paths_manager::get_paths().unwrap(); // make error handler 
-    print!("{}", paths.zapret)
+    let file = Command::new(r"C:\Users\UglyASF\Desktop\test.txt")
+        .status()
+        .expect("smth gone wrong");
+
+    println!("hehe: {}", file)
 }
