f352090f33a1cdccbeaf6f926f0bc4e1e76e4a90
lonely43 <iladenisov327@gmail.com>
2025-08-20 14:47:21 +0300

    fix: pizdec

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ mod models;
 mod config;
 mod app;
 
+use std::thread;
 use std::process::Command;
+use std::time::Duration;
 
 use crate::models::Paths;
 use crate::app::get_paths;
@@ -20,14 +22,12 @@ pub fn init() {
 
 fn start(paths: &Paths) {
    let mut processes = vec![
-      Command
-         ::new("cmd")
+      Command::new("cmd")
          .args(["/C", "start", &paths.zapret_path])
          .spawn()
          .expect("Failed to open file"),
 
-      Command
-         ::new("cmd")
+      Command::new("cmd")
          .args(["/C", "start", &paths.discord_path])
          .spawn()
          .expect("Failed to open file")
@@ -35,5 +35,7 @@ fn start(paths: &Paths) {
 
    while processes.len() > 0 {
       processes.retain_mut(|process| matches!(process.try_wait(), Ok(Some(_))));
+
+      thread::sleep(Duration::from_millis(50))
    }
 }
